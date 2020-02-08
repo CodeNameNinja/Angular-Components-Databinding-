7f8bf9667932eb47292eeff7014945650ff4c4bf
Mitchell Yuen <35192626+CodeNameNinja@users.noreply.github.com>
2020-02-08 21:42:41 +0200

    created cockpit and server-element component and used custome binding

---
+++ src/app/server-element/server-element.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { ServerElement } from './server-element.model';
+
+@Component({
+  selector: 'app-server-element',
+  templateUrl: './server-element.component.html',
+  styleUrls: ['./server-element.component.css']
+})
+export class ServerElementComponent implements OnInit {
+  @Input() element: ServerElement;
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
