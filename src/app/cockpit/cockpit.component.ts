63c8a8c3c59dbb7bb46153506153c93350191756
Mitchell Yuen <35192626+CodeNameNinja@users.noreply.github.com>
2020-02-08 22:47:21 +0200

    using local references instead of two way binding

--- src/app/cockpit/cockpit.component.ts
+++ src/app/cockpit/cockpit.component.ts
@@ -9,17 +9,22 @@ import { Component, OnInit, Output,  EventEmitter } from '@angular/core';
 export class CockpitComponent implements OnInit {
   @Output() serverCreated = new EventEmitter<{serverName: string, serverContent: string}>();
   @Output() bluePrintCreated = new EventEmitter<{serverName: string, serverContent: string}>();
-  newServerName = '';
-  newServerContent = '';
+
   constructor() { }
 
   ngOnInit() {
   }
-  onAddServer() {
-    this.serverCreated.emit({serverName: this.newServerName, serverContent: this.newServerContent});
+  onAddServer(nameInput: HTMLInputElement, serverContentInput: HTMLInputElement) {
+    this.serverCreated.emit({
+      serverName: nameInput.value,
+      serverContent: serverContentInput.value
+    });
   }
 
-  onAddBlueprint() {
-    this.bluePrintCreated.emit({serverName: this.newServerName, serverContent: this.newServerContent});
+  onAddBlueprint(nameInput: HTMLInputElement, serverContentInput: HTMLInputElement) {
+    this.bluePrintCreated.emit({
+      serverName: nameInput.value,
+      serverContent: serverContentInput.value,
+    });
   }
 }
